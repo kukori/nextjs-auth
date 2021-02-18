929fc433c669a85f6030d558550ec3e2e8f4bfc3
kukori <attila.varjas@gmail.com>
2021-02-18 11:03:53 +0700

    Setting up express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const next = require("next");
+const express = require("express");
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3000;
+const app = next({ dev });
+
+app.prepare().then(() => {
+    const server = express();
+
+    server.listen(port, error => {
+        if (error) {
+            throw error;
+        }
+        console.log(`Listening on port ${port}`);
+    })
+})
